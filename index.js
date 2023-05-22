b8c99a54475de4c80f07c12b5114dc5d896e5480
lech1n <110564512+lech1n@users.noreply.github.com>
2023-05-22 21:10:27 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+function generateScheme(){
+	const colorPicker = document.getElementById('color-picker').value.substr(-6)
+	const options = document.getElementById('select').value
+
+	fetch(`https://www.thecolorapi.com/scheme?hex=${colorPicker}&mode=${options}`)
+	.then(response => response.json())
+	.then((data)=>{
+
+		let section = document.querySelector("section")
+		section.innerHTML = ''
+
+		for(let scheme of data.colors){
+			section.innerHTML+= `
+			<div class="containers">
+			<img src=${scheme.image.bare}>
+			<div class="h3-container">
+			<h3>${scheme.hex.value}</h3>
+			</div>
+			</div>
+			`
+		}
+	})}
+
+
